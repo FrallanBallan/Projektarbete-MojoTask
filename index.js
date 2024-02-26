bcca5360c2685088b4e80ed0ca076fb7e593d209
Simon Kallkvist <simon.kallkvist@gmail.com>
2024-02-26 09:51:54 +0100

    Angelo Fixed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//Login - createAccount start - Angelo divarna;
 //Login - createAccount start - Angelo
-divarna;
+//divarna
 let LoginForm = document.querySelector('.Login-form');
 let CreateForm = document.querySelector('.CreateAccount-form');
-knapparna;
+//knapparna
 let BtnToCreate = document.querySelector('#CreateBtn');
 let CreateAccountBtn = document.querySelector('#CreateAccountBtn');
 let AlreadyAccountBtn = document.querySelector('#alredyaccount');
+
 let LoggainBtn = document.querySelector('#LoginBtn');
 //inputs
 let Newuser = document.querySelector('#Createusername');
@@ -37,14 +39,16 @@ function LoginClose() {
 
   CreateForm.classList.add('focus');
   CreateForm.classList.remove('hidden');
+  NollställLogin();
 }
 //funktionen som öppnar tillbaka Login och stänger create
 function LoginOpen() {
-  CreateForm.classList.add('hidden');
-  CreateForm.classList.remove('focus');
-
   LoginForm.classList.add('focuss');
   LoginForm.classList.remove('hidden');
+
+  CreateForm.classList.remove('focus');
+  CreateForm.classList.add('hidden');
+  NollställLogin();
 }
 //knappen som man går till CreateAccountform för att skapa ett konto
 BtnToCreate.addEventListener('click', () => {
@@ -53,10 +57,48 @@ BtnToCreate.addEventListener('click', () => {
 //knappen som man skapar ett konto på
 CreateAccountBtn.addEventListener('click', () => {
   LoginOpen();
+  //om lösen input och reapeatlösen input matchar gå vidare
+  if (NewLösen.value === RepeatLösen.value) {
+    if (
+      //om användarnamnetin har onödiga mellanslag i sig ta bort dom trim()
+      Newuser.value.trim() !== '' &&
+      NewLösen.value.trim() !== '' &&
+      //kolla så att users inte redan har ett value med samma användarnamn value
+      !users.hasOwnProperty(Newuser.value)
+    ) {
+      //spara i localstorage
+      users[Newuser.value] = NewLösen.value;
+      localStorage.setItem('users', JSON.stringify(users));
+      LoginOpen();
+      //annars skriv ut felmeddelande
+    } else {
+      felmessage.innerText = 'Användarnamn upptaget eller fältet tomt!';
+    }
+    //annars om lösenorden inte matchar varandra skriv ut error meddelande
+  } else {
+    RepeatLösen.classList.add('errorInput');
+    felmessage.innerText = 'Lösenordet matchar inte';
+  }
 });
 //knappen jag har redan ett konto stänger createAccount och öppnar login
 AlreadyAccountBtn.addEventListener('click', () => {
   LoginOpen();
+});
+
+LoggainBtn.addEventListener('click', () => {
+  //hämtar sessionstorage med keyn alla users och lägger in detta i en variabel
+  let storedUsers = JSON.parse(localStorage.getItem('users'));
+  //om storedUsers hadownpropery(kollar så att objektet innehåller en specifik egenskap såg den kollar om storeduser innehåller likadant användarnamn som användaren har stoppat in i inputen)
+  if (
+    storedUsers.hasOwnProperty(Namn.value) &&
+    storedUsers[Namn.value] === Lösenord.value
+  ) {
+    console.log('hej');
+    NollställLogin();
+  } else {
+    //misslyckades att logga in
+    p.innerText = 'Felaktigt användarnamn eller lösenord!';
+  }
 });
 
 // Login - CreateAccount End Angelo
