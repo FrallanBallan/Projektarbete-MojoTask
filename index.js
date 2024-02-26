d9354211726d4e72dc57b68f1d1e264cf03206ce
Simon Kallkvist <simon.kallkvist@gmail.com>
2024-02-26 09:35:56 +0100

    Main merge

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-// //Login - createAccount start - Angelo
-//divarna
-// let LoginForm = document.querySelector('.Login-form');
-// let CreateForm = document.querySelector('.CreateAccount-form');
-//knapparna
-// let BtnToCreate = document.querySelector('#CreateBtn');
-// let CreateAccountBtn = document.querySelector('#CreateAccountBtn');
-// let AlreadyAccountBtn = document.querySelector('#alredyaccount');
-let LoggainBtn = document.querySelector("#LoginBtn");
+//Login - createAccount start - Angelo
+divarna;
+let LoginForm = document.querySelector('.Login-form');
+let CreateForm = document.querySelector('.CreateAccount-form');
+knapparna;
+let BtnToCreate = document.querySelector('#CreateBtn');
+let CreateAccountBtn = document.querySelector('#CreateAccountBtn');
+let AlreadyAccountBtn = document.querySelector('#alredyaccount');
+let LoggainBtn = document.querySelector('#LoginBtn');
 //inputs
-let Newuser = document.querySelector("#Createusername");
-let NewLösen = document.querySelector("#Createpassword");
-let RepeatLösen = document.querySelector("#repeatpassword");
-let Namn = document.querySelector("#username");
-let Lösenord = document.querySelector("#password");
+let Newuser = document.querySelector('#Createusername');
+let NewLösen = document.querySelector('#Createpassword');
+let RepeatLösen = document.querySelector('#repeatpassword');
+let Namn = document.querySelector('#username');
+let Lösenord = document.querySelector('#password');
 //objekt users som lagrar alla users
 let users = {};
 //felmeddelande p-tagar
-let p = document.querySelector("#error");
-let felmessage = document.querySelector(".felmeddelande");
+let p = document.querySelector('#error');
+let felmessage = document.querySelector('.felmeddelande');
 //nollställer alla inputs fält och tagar
 function NollställLogin() {
   //nollställer inputsfälten
-  Newuser.value = "";
-  NewLösen.value = "";
-  RepeatLösen.value = "";
-  Namn.value = "";
-  Lösenord.value = "";
-  p.innerText = "";
-  felmessage.innerText = "";
-  RepeatLösen.classList.remove("errorInput");
+  Newuser.value = '';
+  NewLösen.value = '';
+  RepeatLösen.value = '';
+  Namn.value = '';
+  Lösenord.value = '';
+  p.innerText = '';
+  felmessage.innerText = '';
+  RepeatLösen.classList.remove('errorInput');
 }
-// //funktionen som öppnar upp create-form och gömmer login-form
-// function LoginClose() {
-//   LoginForm.classList.add('hidden');
-//   LoginForm.classList.remove('focuss');
+//funktionen som öppnar upp create-form och gömmer login-form
+function LoginClose() {
+  LoginForm.classList.add('hidden');
+  LoginForm.classList.remove('focuss');
 
-//   CreateForm.classList.add('focus');
-//   CreateForm.classList.remove('hidden');
-// }
-// //funktionen som öppnar tillbaka Login och stänger create
-// function LoginOpen() {
-//   CreateForm.classList.add('hidden');
-//   CreateForm.classList.remove('focus');
+  CreateForm.classList.add('focus');
+  CreateForm.classList.remove('hidden');
+}
+//funktionen som öppnar tillbaka Login och stänger create
+function LoginOpen() {
+  CreateForm.classList.add('hidden');
+  CreateForm.classList.remove('focus');
 
-//   LoginForm.classList.add('focuss');
-//   LoginForm.classList.remove('hidden');
-// }
-// //knappen som man går till CreateAccountform för att skapa ett konto
-// BtnToCreate.addEventListener('click', () => {
-//   LoginClose();
-// });
-// //knappen som man skapar ett konto på
-// CreateAccountBtn.addEventListener('click', () => {
-//   LoginOpen();
-// });
-// //knappen jag har redan ett konto stänger createAccount och öppnar login
-// AlreadyAccountBtn.addEventListener('click', () => {
-//   LoginOpen();
-// });
+  LoginForm.classList.add('focuss');
+  LoginForm.classList.remove('hidden');
+}
+//knappen som man går till CreateAccountform för att skapa ett konto
+BtnToCreate.addEventListener('click', () => {
+  LoginClose();
+});
+//knappen som man skapar ett konto på
+CreateAccountBtn.addEventListener('click', () => {
+  LoginOpen();
+});
+//knappen jag har redan ett konto stänger createAccount och öppnar login
+AlreadyAccountBtn.addEventListener('click', () => {
+  LoginOpen();
+});
 
 // Login - CreateAccount End Angelo
 
@@ -65,29 +65,29 @@ function NollställLogin() {
 
 // Globals
 
-let contentContainer = document.querySelector(".contentContainer");
-let homeTab = document.querySelector("#homeTab");
-let todoTab = document.querySelector("#todoTab");
-let habitsTab = document.querySelector("#habitsTab");
-let timerTab = document.querySelector("#timerTab");
-let calenderTab = document.querySelector("#calenderTab");
-let weatherTab = document.querySelector("#weatherTab");
+let contentContainer = document.querySelector('.contentContainer');
+let homeTab = document.querySelector('#homeTab');
+let todoTab = document.querySelector('#todoTab');
+let habitsTab = document.querySelector('#habitsTab');
+let timerTab = document.querySelector('#timerTab');
+let calenderTab = document.querySelector('#calenderTab');
+let weatherTab = document.querySelector('#weatherTab');
 
 // Global Arrays
 let todoList = []; // JSON.parse(localStorage.getItem('links'));
 
 function showHome() {
   //removing styles and content
-  contentContainer.innerHTML = "";
-  todoTab.removeAttribute("style");
-  habitsTab.removeAttribute("style");
-  timerTab.removeAttribute("style");
-  calenderTab.removeAttribute("style");
-  weatherTab.removeAttribute("style");
+  contentContainer.innerHTML = '';
+  todoTab.removeAttribute('style');
+  habitsTab.removeAttribute('style');
+  timerTab.removeAttribute('style');
+  calenderTab.removeAttribute('style');
+  weatherTab.removeAttribute('style');
   //removing styles and content
 
-  homeTab.style.width = "110%";
-  homeTab.style.borderRadius = "0% 10% 10% 0%";
+  homeTab.style.width = '110%';
+  homeTab.style.borderRadius = '0% 10% 10% 0%';
 
   contentContainer.innerHTML = `
   <div class="homeCardContainer">
@@ -129,11 +129,11 @@ function showHome() {
 </div>
   `;
   // Render li from todoList and show on home page -start
-  let todoUl = document.querySelector(".overviewList");
+  let todoUl = document.querySelector('.overviewList');
   todoList.forEach((todo) => {
     console.log(todo);
 
-    let todoLi = document.createElement("li");
+    let todoLi = document.createElement('li');
     todoLi.innerText = todo.title;
     todoUl.append(todoLi);
   });
@@ -143,15 +143,15 @@ function showHome() {
 
 function showTodos() {
   //removing styles and content
-  contentContainer.innerHTML = "";
-  homeTab.removeAttribute("style");
-  habitsTab.removeAttribute("style");
-  timerTab.removeAttribute("style");
-  calenderTab.removeAttribute("style");
-  weatherTab.removeAttribute("style");
+  contentContainer.innerHTML = '';
+  homeTab.removeAttribute('style');
+  habitsTab.removeAttribute('style');
+  timerTab.removeAttribute('style');
+  calenderTab.removeAttribute('style');
+  weatherTab.removeAttribute('style');
   //removing styles and content
-  todoTab.style.width = "110%";
-  todoTab.style.borderRadius = "0% 10% 10% 0%";
+  todoTab.style.width = '110%';
+  todoTab.style.borderRadius = '0% 10% 10% 0%';
   contentContainer.innerHTML = `
   <div class="homeTodoTab">
   <h2>Your todo's</h2>
@@ -173,27 +173,27 @@ function showTodos() {
 `;
 
   let todoContainerUnfinished = document.querySelector(
-    "#todoContainerUnfinished"
+    '#todoContainerUnfinished'
   );
   let todoCardContainerFinished = document.querySelector(
-    "#todoCardContainerFinished"
+    '#todoCardContainerFinished'
   );
 
   // Print the todo Cards from todoList that gets from local storage start
 
   todoList.forEach((todoObject) => {
     //New Verson
-    let todoCard = document.createElement("div");
-    todoCard.classList.add("card");
-    let todoTitle = document.createElement("p");
+    let todoCard = document.createElement('div');
+    todoCard.classList.add('card');
+    let todoTitle = document.createElement('p');
     todoTitle.innerText = todoObject.title;
-    let todoCategory = document.createElement("div");
-    todoCategory.classList.add("todoCategory");
-    if (todoObject.category === "Home") {
+    let todoCategory = document.createElement('div');
+    todoCategory.classList.add('todoCategory');
+    if (todoObject.category === 'Home') {
       todoCategory.innerHTML = `<i class="fa-solid fa-house"></i>`;
-    } else if (todoObject.category === "School") {
+    } else if (todoObject.category === 'School') {
       todoCategory.innerHTML = `<i class="fa-solid fa-school"></i>`;
-    } else if (todoObject.category === "Training") {
+    } else if (todoObject.category === 'Training') {
       todoCategory.innerHTML = `<i class="fa-solid fa-dumbbell"></i>`;
     } else {
       todoCategory.innerHTML = `<i class="fa-solid fa-hand-sparkles"></i>`;
@@ -206,21 +206,21 @@ function showTodos() {
 
     todoCard.append(todoTitle, todoCategory);
 
-    if (todoObject.status === "unfinished") {
-      todoCard.classList.add("unfinished");
+    if (todoObject.status === 'unfinished') {
+      todoCard.classList.add('unfinished');
       todoContainerUnfinished.append(todoCard);
     } else {
-      todoCard.classList.add("finished");
+      todoCard.classList.add('finished');
       todoCardContainerFinished.append(todoCard);
     }
   });
   // Print the todo Cards from todoList that gets from local storage end
 
   // add eventlistener for removing todoCards start
-  let removeBtns = document.querySelectorAll(".removeBtn");
+  let removeBtns = document.querySelectorAll('.removeBtn');
 
   Array.from(removeBtns).forEach((removeBtn) => {
-    removeBtn.addEventListener("click", () => {
+    removeBtn.addEventListener('click', () => {
       todoList.forEach((todoObject) => {
         if (removeBtn.parentElement.innerHTML.includes(todoObject.title)) {
           let index = todoList.findIndex((todoObject) =>
@@ -241,15 +241,15 @@ function showTodos() {
 
 function showHabits() {
   //removing styles and content
-  contentContainer.innerHTML = "";
-  todoTab.removeAttribute("style");
-  homeTab.removeAttribute("style");
-  timerTab.removeAttribute("style");
-  calenderTab.removeAttribute("style");
-  weatherTab.removeAttribute("style");
+  contentContainer.innerHTML = '';
+  todoTab.removeAttribute('style');
+  homeTab.removeAttribute('style');
+  timerTab.removeAttribute('style');
+  calenderTab.removeAttribute('style');
+  weatherTab.removeAttribute('style');
   //removing styles and content
-  habitsTab.style.width = "110%";
-  habitsTab.style.borderRadius = "0% 10% 10% 0%";
+  habitsTab.style.width = '110%';
+  habitsTab.style.borderRadius = '0% 10% 10% 0%';
 
   contentContainer.innerHTML = `
   <div class="habitContainer">
@@ -282,51 +282,51 @@ function showHabits() {
 
 function showTimer() {
   //removing styles and content
-  contentContainer.innerHTML = "";
-  todoTab.removeAttribute("style");
-  habitsTab.removeAttribute("style");
-  homeTab.removeAttribute("style");
-  calenderTab.removeAttribute("style");
-  weatherTab.removeAttribute("style");
+  contentContainer.innerHTML = '';
+  todoTab.removeAttribute('style');
+  habitsTab.removeAttribute('style');
+  homeTab.removeAttribute('style');
+  calenderTab.removeAttribute('style');
+  weatherTab.removeAttribute('style');
   //removing styles and content
-  timerTab.style.width = "110%";
-  timerTab.style.borderRadius = "0% 10% 10% 0%";
-  let test = document.createElement("h1");
-  test.innerHTML = "estoy testo timer";
+  timerTab.style.width = '110%';
+  timerTab.style.borderRadius = '0% 10% 10% 0%';
+  let test = document.createElement('h1');
+  test.innerHTML = 'estoy testo timer';
 
   contentContainer.append(test);
 }
 
 function showCalender() {
   //removing styles and content
-  contentContainer.innerHTML = "";
-  todoTab.removeAttribute("style");
-  habitsTab.removeAttribute("style");
-  timerTab.removeAttribute("style");
-  homeTab.removeAttribute("style");
-  weatherTab.removeAttribute("style");
+  contentContainer.innerHTML = '';
+  todoTab.removeAttribute('style');
+  habitsTab.removeAttribute('style');
+  timerTab.removeAttribute('style');
+  homeTab.removeAttribute('style');
+  weatherTab.removeAttribute('style');
   //removing styles and content
-  calenderTab.style.width = "110%";
-  calenderTab.style.borderRadius = "0% 10% 10% 0%";
-  let test = document.createElement("h1");
-  test.innerHTML = "estoy testo caldendoro";
+  calenderTab.style.width = '110%';
+  calenderTab.style.borderRadius = '0% 10% 10% 0%';
+  let test = document.createElement('h1');
+  test.innerHTML = 'estoy testo caldendoro';
 
   contentContainer.append(test);
 }
 
 function showWeather() {
   //removing styles and content
-  contentContainer.innerHTML = "";
-  todoTab.removeAttribute("style");
-  habitsTab.removeAttribute("style");
-  timerTab.removeAttribute("style");
-  calenderTab.removeAttribute("style");
-  homeTab.removeAttribute("style");
+  contentContainer.innerHTML = '';
+  todoTab.removeAttribute('style');
+  habitsTab.removeAttribute('style');
+  timerTab.removeAttribute('style');
+  calenderTab.removeAttribute('style');
+  homeTab.removeAttribute('style');
   //removing styles and content
-  weatherTab.style.width = "110%";
-  weatherTab.style.borderRadius = "0% 10% 10% 0%";
-  let test = document.createElement("h1");
-  test.innerHTML = "estoy testo hace calor?";
+  weatherTab.style.width = '110%';
+  weatherTab.style.borderRadius = '0% 10% 10% 0%';
+  let test = document.createElement('h1');
+  test.innerHTML = 'estoy testo hace calor?';
 
   contentContainer.append(test);
 }
@@ -337,7 +337,7 @@ function showLogout() {}
 
 // Funktioner som genererar content- start
 function createTodo() {
-  contentContainer.innerHTML = "";
+  contentContainer.innerHTML = '';
 
   contentContainer.innerHTML = `
   <div class="todoInfo">
@@ -396,36 +396,36 @@ function createTodo() {
 
   `;
 
-  let categoryChoice = document.querySelector("#categoryChoice");
+  let categoryChoice = document.querySelector('#categoryChoice');
   console.log(categoryChoice);
 
-  let saveBtn = document.querySelector("#saveBtn");
+  let saveBtn = document.querySelector('#saveBtn');
 
-  saveBtn.addEventListener("click", () => {
+  saveBtn.addEventListener('click', () => {
     let todoObject = {};
     if (
-      document.querySelector("#whatTodo").value &&
-      document.querySelector("#descTodo").value &&
-      document.querySelector("#deadlineTodo").value &&
-      document.querySelector("#categoryChoice").value &&
+      document.querySelector('#whatTodo').value &&
+      document.querySelector('#descTodo').value &&
+      document.querySelector('#deadlineTodo').value &&
+      document.querySelector('#categoryChoice').value &&
       document.querySelector('input[name="TodoChoice"]:checked').value &&
-      document.querySelector("#categoryChoice").value !== "Choose one"
+      document.querySelector('#categoryChoice').value !== 'Choose one'
     ) {
-      todoObject.id = "testPerson";
-      todoObject.title = document.querySelector("#whatTodo").value;
-      todoObject.desc = document.querySelector("#descTodo").value;
-      todoObject.deadline = document.querySelector("#deadlineTodo").value;
-      todoObject.category = document.querySelector("#categoryChoice").value;
+      todoObject.id = 'testPerson';
+      todoObject.title = document.querySelector('#whatTodo').value;
+      todoObject.desc = document.querySelector('#descTodo').value;
+      todoObject.deadline = document.querySelector('#deadlineTodo').value;
+      todoObject.category = document.querySelector('#categoryChoice').value;
       todoObject.status = document.querySelector(
         'input[name="TodoChoice"]:checked'
       ).value;
       console.log(todoObject);
       todoList.push(todoObject);
       saveTodoData();
-      alert("A new Todo has been added!");
+      alert('A new Todo has been added!');
       showTodos();
     } else {
-      alert("Check to make sure all fields has been filled!");
+      alert('Check to make sure all fields has been filled!');
     }
   });
 }
@@ -444,11 +444,11 @@ function removeTodo() {
 
 function saveTodoData() {
   // console.log(todoList);
-  localStorage.setItem("todoData", JSON.stringify(todoList));
+  localStorage.setItem('todoData', JSON.stringify(todoList));
 }
 
 function getTodoData() {
-  todoList = JSON.parse(localStorage.getItem("todoData"));
+  todoList = JSON.parse(localStorage.getItem('todoData'));
 }
 
 getTodoData();
