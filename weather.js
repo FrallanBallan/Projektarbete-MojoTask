2e8de308f79a477247698975f3eaeb7c1e974209
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-02-29 15:17:44 +0100

    start of weather app

--- weather.js
+++ weather.js
@@ -1,4 +1,7 @@
-//API
+d; //API
+const apiKey = "57a622d1a38b6d1497b9a19a259dfdea";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 //GLOBALS
 
@@ -14,8 +17,46 @@ function showWeather() {
   weatherTab.style.width = "110%";
   weatherTab.style.borderRadius = "0% 10% 10% 0%";
 
-  let test = document.createElement("h1");
-  test.innerHTML = "estoy testo hace cadddddddlor?";
+  let weatherDiv = document.createElement("div");
+  weatherDiv.classList.add("weatherDiv");
 
-  contentContainer.append(test);
+  let weatherTitle = document.createElement("h2");
+  weatherTitle.innerText = "Weather Forecast";
+
+  let weatherSearch = document.createElement("div");
+  weatherSearch.classList.add("weatherSearch");
+  weatherSearch.innerHTML = `
+  <input
+      type = "text"
+      name = "cityWeather"
+      id = "weatherInput"
+      placeholder = "Enter city name"
+      />
+  <button class="search"><img src="images/search.png" ></button>
+  `;
+
+  let weatherResult = document.createElement("div");
+  weatherResult.classList.add("weatherResult");
+
+  weatherResult.innerHTML = `
+    <div class="weatherResult> 
+  <h2 id="city"> Stockholm </h2>
+  <h2 id="temp"> 7°C </h2>
+  </div>
+  
+  `;
+  weatherDiv.append(weatherTitle, weatherSearch, weatherResult);
+  contentContainer.append(weatherDiv);
+}
+
+//  <h1> Weather </h1>
+//   <div class="divider"></div>
+//     <div class="weatherDiv"
+//     <label for= "searchWeather"> Weather forecaste in: </label>
+//     <input type= "text" placeholder= "Sweden, Stockholm"></input>
+//     </div>
+//   `
+{
+  /* <input type="text" placeholder="Enter city name" id="weatherInput" />
+  <button><img src="images/search.png" alt="search"></button>  */
 }
