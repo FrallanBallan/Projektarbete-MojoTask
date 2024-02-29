28b4f16d4e60e20942b2498beb9db22159b95dd4
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-02-29 11:56:55 +0100

    added js page for mr sure

--- calender.js
+++ calender.js
@@ -1,1 +1,17 @@
 // -_- Mr Sure
+function showCalender() {
+  //removing styles and content
+  contentContainer.innerHTML = "";
+  todoTab.removeAttribute("style");
+  habitsTab.removeAttribute("style");
+  timerTab.removeAttribute("style");
+  homeTab.removeAttribute("style");
+  weatherTab.removeAttribute("style");
+  //removing styles and content
+  calenderTab.style.width = "110%";
+  calenderTab.style.borderRadius = "0% 10% 10% 0%";
+  let test = document.createElement("h1");
+  test.innerHTML = "estoy testo caldendoro";
+
+  contentContainer.append(test);
+}
