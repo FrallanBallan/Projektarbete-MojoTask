c76327fa5d3e8e7482835adc3f5599fdb42660ce
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-02-29 12:10:07 +0100

    test

--- scriptHome.js
+++ scriptHome.js
@@ -538,23 +538,6 @@ function sortPriorites(list, value) {
 }
 // Functions for filter and sort habits - end
 
-function showCalender() {
-  //removing styles and content
-  contentContainer.innerHTML = "";
-  todoTab.removeAttribute("style");
-  habitsTab.removeAttribute("style");
-  timerTab.removeAttribute("style");
-  homeTab.removeAttribute("style");
-  weatherTab.removeAttribute("style");
-  //removing styles and content
-  calenderTab.style.width = "110%";
-  calenderTab.style.borderRadius = "0% 10% 10% 0%";
-  let test = document.createElement("h1");
-  test.innerHTML = "estoy testo caldendoro";
-
-  contentContainer.append(test);
-}
-
 function showLogout() {}
 
 // Funktioner för sidorientering - End Frans
